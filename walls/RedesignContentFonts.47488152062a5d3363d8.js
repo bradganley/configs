ae407bdff2b2d7ed0cfc1ee6ba8d41ab8edac92e
Brad Ganley <bradart.ink@gmail.com>
2020-09-19 18:24:45 -0500

    too much bullshit

--- walls/RedesignContentFonts.47488152062a5d3363d8.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([["RedesignContentFonts"],{"./assets/fonts/NotoMono/font.less":function(n,o,s){},"./assets/fonts/NotoSans/font.less":function(n,o,s){}}]);
-//# sourceMappingURL=RedesignContentFonts.47488152062a5d3363d8.js.map
--- walls/RedesignSystemFonts.cd867adce92c628ca246.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([["RedesignSystemFonts"],{"./assets/fonts/IBMPlexSans/font.less":function(s,n,e){}}]);
-//# sourceMappingURL=RedesignSystemFonts.cd867adce92c628ca246.js.map
