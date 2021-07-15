34066227ccd312c31b21040a01becedee84a6650
Brad Ganley <bradganley@protonmail.com>
2021-07-15 13:38:42 -0500

    removed broken script file

--- RCconfig.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-mv ~/.bashrc ~/bak.bashrc
-mv ~/.bash_aliases ~/bak.bash_aliases
-ln -s .bashrc ~/.bashrc
-ln -s .bash_aliases ~/.bash_aliases
-chmod 777 .bashrc
-chmod 777.bash_aliases
-source .bashrc
